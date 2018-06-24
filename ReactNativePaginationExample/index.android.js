db4909eea448e861a4878557c682b78ed5717990
garrettmac <garrettmacmac@gmail.com>
2018-06-23 18:47:07 -0700

    added linting and created eslint and gitignore

---
+++ ReactNativePaginationExample/index.android.js
@@ -0,0 +1,10 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+import App from './App.js';
+// Not Tested
+AppRegistry.registerComponent('ReactNativePaginationExample', () => App);
